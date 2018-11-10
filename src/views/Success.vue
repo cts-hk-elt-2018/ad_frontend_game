9bf6c995ec3e73137f53541aa7f05da04a86be7c
ckho <me@ckho.info>
2018-11-10 22:40:22 +0800

    Success Page

---
+++ src/views/Success.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="success">
+    <div class="main">
+      <div class="container">
+        <div class="wizard">
+          <div class="content">
+            <!-- <p>Please upload the photo</p> -->
+            <p id="success">Success</p>
+          </div>
+          <div class="action">
+            <button v-on:click="next();">
+              <svg style="width:60%;height:60%" viewBox="0 0 24 24">
+                <path fill="#FFFFFF" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cover cover3">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "success",
+  data() {
+    return {
+      groupname: null
+    };
+  },
+  created() {
+    if (this.$route.query.groupname) {
+      this.groupname = this.$route.query.groupname;
+    }
+  },
+  methods: {
+    next() {
+      this.$router.push({ name: 'upload', query: { groupname: this.groupname } });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.cover3 {
+  width: 100%;
+  height: 100vh;
+  background: #0033a0;
+  position: absolute;
+  z-index: 0;
+  bottom: 0px;
+}
+#success {
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: transparent;
+  font-size: 4vh;
+  font-weight: bold;
+  box-sizing: border-box;
+  font-family: 'Montserrat';
+  padding-top: 30%;
+  padding-bottom: 5%;
+  outline: none;
+  appearance: unset !important;
+  -moz-appearance: unset !important;
+  -webkit-appearance: unset !important;
+  -o-appearance: unset !important;
+  -ms-appearance: unset !important;
+  -webkit-appearance: none;
+  transition: 0.3s ease;
+  -moz-transition: 0.3s ease;
+  -webkit-transition: 0.3s ease;
+  -o-transition: 0.3s ease;
+  -ms-transition: 0.3s ease;
+  bottom: 0px;
+}
+</style>
