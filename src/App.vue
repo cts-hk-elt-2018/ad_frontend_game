e9c410453f8b1257743cbf81ac5f15a67623f8d4
ckho <me@ckho.info>
2018-11-18 22:41:10 +0800

    Add Cognizant Logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view />
+    <img class="logo" src="./assets/cognizant_logo_white.png" />
   </div>
 </template>
 
@@ -12,5 +13,11 @@
   text-align: center;
   background: #00a7f6;
   height: 100vh;
+}
+.logo {
+  position: fixed;
+  bottom: 20px;
+  max-width: 50vw;
+  z-index: 999;
 }
 </style>
